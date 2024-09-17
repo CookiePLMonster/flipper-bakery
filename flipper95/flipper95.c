2bbe8a3bbeae8284f210ceea20503063baa3943a
Silent <zdanio95@gmail.com>
2024-09-17 21:18:07 +0200

    Flipper95: Cli command to advance to a specific number + pretty rendering

--- flipper95/flipper95.c
+++ flipper95/flipper95.c
@@ -1,18 +1,32 @@
 #include <furi.h>
 #include <gui/gui.h>
 #include <input/input.h>
+#include <cli/cli.h>
+#include <toolbox/args.h>
+#include <toolbox/strint.h>
+
+#include <furi_hal_power.h>
 
 #include <stdatomic.h>
 
 #include <mbedtls/bignum.h>
 
-#define TAG "Flipper95"
+#define CLI_COMMAND "flipper95"
 
 typedef struct {
     FuriPubSub* input;
     FuriPubSubSubscription* input_subscription;
     Gui* gui;
     Canvas* canvas;
+    Cli* cli;
+
+    // These are protected by the mutex
+    FuriMutex* state_mutex;
+    uint32_t cur_mnumber; // Current Mersenne number analyzed
+    uint32_t cur_mprime; // Last Mersenne prime found
+    char* mprime_str;
+    size_t mprime_str_len;
+
     atomic_bool stop;
 } Flipper95;
 
@@ -38,34 +52,83 @@ static bool thread_signal_callback(uint32_t signal, void* arg, void* context) {
     return false;
 }
 
-static Flipper95* flipper95_alloc(void) {
-    Flipper95* instance = malloc(sizeof(Flipper95));
+static void flipper95_cli_print_usage() {
+    printf("Stress test your Flipper by calculating Mersenne primes, same as Prime95 does\r\n"
+           "\r\n"
+           "Usage:\r\n" CLI_COMMAND " <cmd> <args>\r\n"
+           "Cmd list:\r\n"
+           "\tadvance M<p:int> - Advance calculations to a Mersenne number M_p\r\n"
+           "\tprint\t\t - Print the last found Mersenne prime\r\n");
+}
 
+static bool flipper95_cli_set_mnumber(const FuriString* args, Flipper95* instance) {
+    // TODO: furi_string_start_withi when it's available
+    if(furi_string_size(args) > 2 && furi_string_start_with(args, "M")) {
+        uint32_t number;
+        if(strint_to_uint32(furi_string_get_cstr(args) + 1, NULL, &number, 10) ==
+           StrintParseNoError) {
+            furi_check(furi_mutex_acquire(instance->state_mutex, FuriWaitForever) == FuriStatusOk);
+            instance->cur_mnumber = number;
+            furi_check(furi_mutex_release(instance->state_mutex) == FuriStatusOk);
+            return true;
+        }
+    }
+    return false;
+}
+
+static void flipper95_cli_callback(Cli* cli, FuriString* args, void* context) {
+    UNUSED(cli);
+    Flipper95* instance = context;
+
+    FuriString* cmd = furi_string_alloc();
+    bool success = false;
+    if(args_read_string_and_trim(args, cmd)) {
+        if(furi_string_equal(cmd, "advance")) {
+            success = flipper95_cli_set_mnumber(args, instance);
+        }
+    }
+
+    if(!success) {
+        flipper95_cli_print_usage();
+    }
+}
+
+static void flipper95_init(Flipper95* instance) {
     instance->input = furi_record_open(RECORD_INPUT_EVENTS);
     instance->gui = furi_record_open(RECORD_GUI);
     instance->canvas = gui_direct_draw_acquire(instance->gui);
+    instance->cli = furi_record_open(RECORD_CLI);
 
     instance->input_subscription =
         furi_pubsub_subscribe(instance->input, gui_input_events_callback, instance);
 
-    atomic_init(&instance->stop, false);
+    instance->state_mutex = furi_mutex_alloc(FuriMutexTypeNormal);
+    instance->cur_mnumber = 520;
+    instance->cur_mprime = 0;
+    instance->mprime_str_len = 256;
+    instance->mprime_str = malloc(instance->mprime_str_len);
+    instance->mprime_str[0] = '\0';
 
-    return instance;
+    atomic_init(&instance->stop, false);
 }
 
-static void flipper95_free(Flipper95* instance) {
+static void flipper95_deinit(Flipper95* instance) {
+    free(instance->mprime_str);
+    furi_mutex_free(instance->state_mutex);
+
     furi_pubsub_unsubscribe(instance->input, instance->input_subscription);
 
+    furi_record_close(RECORD_CLI);
     gui_direct_draw_release(instance->gui);
     furi_record_close(RECORD_GUI);
     furi_record_close(RECORD_INPUT_EVENTS);
 }
 
 bool trial_division_is_prime(uint32_t n) {
-    uint32_t sqrt_n = (unsigned int)sqrt((double)n);
-    uint32_t x;
-    if((n % 2) == 0) return 2;
-    for(x = 3; x <= sqrt_n; x += 2) {
+    if(n == 2) return true;
+    if((n % 2) == 0) return false;
+    const uint32_t sqrt_n = (unsigned int)sqrt((double)n);
+    for(uint32_t x = 3; x <= sqrt_n; x += 2) {
         if((n % x) == 0) {
             return false;
         }
@@ -73,26 +136,77 @@ bool trial_division_is_prime(uint32_t n) {
     return true;
 }
 
+static void canvas_draw_ascii_str_wrapped_ellipsis(
+    Canvas* canvas,
+    const int32_t x,
+    const int32_t y,
+    const int32_t wrap_x,
+    const int32_t max_y,
+    const char* str) {
+    const int32_t font_height = 8;
+    int32_t cur_x = x;
+    int32_t cur_y = y;
+
+    bool overflow = false;
+    const int32_t ellipsis_width = canvas_string_width(canvas, "...");
+    while(*str != '\0') {
+        const int32_t glyph_width = canvas_glyph_width(canvas, *str);
+        const bool last_line = cur_y + font_height > max_y;
+        const int32_t max_width_for_line = !last_line ? wrap_x : wrap_x - ellipsis_width;
+        if(cur_x + glyph_width > max_width_for_line) {
+            if(last_line) {
+                overflow = true;
+                break;
+            }
+            cur_x = x;
+            cur_y += font_height;
+        }
+        canvas_draw_glyph(canvas, cur_x, cur_y, *str);
+        cur_x += glyph_width;
+        str++;
+    }
+
+    if(overflow) {
+        canvas_draw_str(canvas, cur_x, cur_y, "...");
+    }
+}
+
 static void flipper95_run(Flipper95* instance) {
     furi_thread_set_current_priority(FuriThreadPriorityIdle);
     furi_thread_set_signal_callback(furi_thread_get_current(), thread_signal_callback, instance);
+    cli_add_command(
+        instance->cli, CLI_COMMAND, CliCommandFlagParallelSafe, flipper95_cli_callback, instance);
+    furi_hal_power_insomnia_enter();
 
     canvas_reset(instance->canvas);
+    canvas_set_font(instance->canvas, FontSecondary);
 
-    uint32_t p = 2;
     mbedtls_mpi M_p, S, temp;
     mbedtls_mpi_init(&M_p);
     mbedtls_mpi_init(&S);
     mbedtls_mpi_init(&temp);
 
-    size_t buffer_len = 256;
-    char* buffer = malloc(buffer_len);
-
     do {
+        // Current Mersenne number may have been advanced by the user via CLI when we weren't looking
+        furi_check(furi_mutex_acquire(instance->state_mutex, FuriWaitForever) == FuriStatusOk);
+        uint32_t p = instance->cur_mnumber;
+        furi_check(furi_mutex_release(instance->state_mutex) == FuriStatusOk);
+        const uint32_t last_number =
+            p; // We'll use this later to check if the user has advanced the calculations themselves
+
         // Take the next prime p
-        do {
+        while(!trial_division_is_prime(p)) {
             ++p;
-        } while(!trial_division_is_prime(p));
+        }
+
+        // Print the canvas BEFORE starting the iteration, so the current number is updated before
+        // we start heavy calculations.
+        char buffer[32];
+        snprintf(buffer, sizeof(buffer), ">M%lu", p);
+        canvas_draw_str_aligned(instance->canvas, 128, 0, AlignRight, AlignTop, buffer);
+
+        canvas_commit(instance->canvas);
+        canvas_clear(instance->canvas);
 
         mbedtls_mpi_lset(&M_p, 1);
         mbedtls_mpi_shift_l(&M_p, p); // 2^p
@@ -109,63 +223,59 @@ static void flipper95_run(Flipper95* instance) {
             mbedtls_mpi_mod_mpi(&S, &temp, &M_p); // S = (S^2 - 2) % M_p
         }
 
-        if(mbedtls_mpi_cmp_int(&S, 0) == 0) {
-            canvas_clear(instance->canvas);
-
-            canvas_set_font(instance->canvas, FontSecondary);
-            canvas_draw_str_aligned(
-                instance->canvas, 1, 0, AlignLeft, AlignTop, "Last prime found:");
-            snprintf(buffer, buffer_len, "M%lu", p);
-            canvas_draw_str_aligned(instance->canvas, 126, 0, AlignRight, AlignTop, buffer);
+        // Now update all the values we usually read from under the lock.
+        // We can render the Mersenne prime without a lock, as this is the only place where it can update.
+        const bool is_prime = mbedtls_mpi_cmp_int(&S, 0) == 0;
+        furi_check(furi_mutex_acquire(instance->state_mutex, FuriWaitForever) == FuriStatusOk);
+        if(instance->cur_mnumber == last_number) {
+            instance->cur_mnumber = p + 1;
+        }
+        if(is_prime) {
+            instance->cur_mprime = p;
 
             size_t required_buffer_len = 0;
-            mbedtls_mpi_write_string(&M_p, 10, buffer, 0, &required_buffer_len);
-            if(required_buffer_len > buffer_len) {
-                free(buffer);
-                buffer = malloc(required_buffer_len);
-                buffer_len = required_buffer_len;
+            mbedtls_mpi_write_string(
+                &M_p, 10, instance->mprime_str, instance->mprime_str_len, &required_buffer_len);
+            if(required_buffer_len > instance->mprime_str_len) {
+                free(instance->mprime_str);
+                instance->mprime_str = malloc(required_buffer_len);
+                instance->mprime_str_len = required_buffer_len;
             }
-            if(mbedtls_mpi_write_string(&M_p, 10, buffer, buffer_len, &required_buffer_len) == 0) {
-                canvas_set_font(instance->canvas, FontKeyboard);
-                const size_t LINE_LENGTH = 21;
-                const size_t MAX_LENGTH = 7 * LINE_LENGTH;
-
-                // Split the text into LINE_LENGTH characters each.
-                // We can fit only 7 lines, so if the string is longer than that, add ellipsis and terminate it early
-                if(required_buffer_len >= MAX_LENGTH) {
-                    strncpy(buffer + MAX_LENGTH - 3, "...", 4);
-                }
-                const char* buffer_ptr = buffer;
-                int32_t y = 8;
-                for(size_t i = 0; i < required_buffer_len && i < MAX_LENGTH; i += LINE_LENGTH) {
-                    char temp_buf[LINE_LENGTH + 1];
-                    strlcpy(temp_buf, buffer_ptr, sizeof(temp_buf));
-
-                    canvas_draw_str_aligned(instance->canvas, 1, y, AlignLeft, AlignTop, temp_buf);
-                    y += 8;
-
-                    buffer_ptr += LINE_LENGTH;
-                }
-            }
-            canvas_commit(instance->canvas);
+            furi_check(
+                mbedtls_mpi_write_string(
+                    &M_p,
+                    10,
+                    instance->mprime_str,
+                    instance->mprime_str_len,
+                    &required_buffer_len) == 0);
         }
+        furi_check(furi_mutex_release(instance->state_mutex) == FuriStatusOk);
+
+        snprintf(buffer, sizeof(buffer), "Last prime: M%lu", instance->cur_mprime);
+        canvas_draw_str_aligned(instance->canvas, 0, 0, AlignLeft, AlignTop, buffer);
+
+        // Render the prime one character at a time, with line breaks and ellipsis
+        canvas_draw_ascii_str_wrapped_ellipsis(
+            instance->canvas, 0, 16, 128, 64, instance->mprime_str);
 
         furi_thread_yield();
     } while(!atomic_load_explicit(&instance->stop, memory_order_relaxed));
 
-    free(buffer);
-
     mbedtls_mpi_free(&M_p);
     mbedtls_mpi_free(&S);
     mbedtls_mpi_free(&temp);
+
+    furi_hal_power_insomnia_exit();
+    cli_delete_command(instance->cli, CLI_COMMAND);
 }
 
 int32_t flipper95_app(void* p) {
     UNUSED(p);
 
-    Flipper95* instance = flipper95_alloc();
-    flipper95_run(instance);
-    flipper95_free(instance);
+    Flipper95 instance;
+    flipper95_init(&instance);
+    flipper95_run(&instance);
+    flipper95_deinit(&instance);
 
     return 0;
 }
